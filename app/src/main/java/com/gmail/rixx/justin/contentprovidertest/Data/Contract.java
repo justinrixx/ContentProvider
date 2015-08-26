f13f96306730d69d23be95d84cf8c1c33488d175
Justin Ricks <justin.rixx@gmail.com>
2015-08-26 15:34:31 -0600

    Initial commit.

---
+++ app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Contract.java
@@ -0,0 +1,12 @@
+package com.gmail.rixx.justin.contentprovidertest.Data;
+
+/**
+ * Stores constants for table names, column names etc.
+ */
+public class Contract {
+
+    public static final String TABLE_MAKES = "makes";
+    public static final String TABLE_CARS  = "cars";
+
+    
+}
