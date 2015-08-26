cbb48ef4154b68635328144b4a72cdf8980e5ef2
Justin Ricks <justin.rixx@gmail.com>
2015-08-26 16:07:41 -0600

    Flesh out the names of the tables and columns in the database

--- app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Contract.java
+++ app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Contract.java
@@ -1,5 +1,7 @@
 package com.gmail.rixx.justin.contentprovidertest.Data;
 
+import android.provider.BaseColumns;
+
 /**
  * Stores constants for table names, column names etc.
  */
@@ -8,5 +10,32 @@ public class Contract {
     public static final String TABLE_MAKES = "makes";
     public static final String TABLE_CARS  = "cars";
 
-    
+    /**
+     * All the constants defined for the makes table in the database
+     */
+    public static final class MakeEntry implements BaseColumns {
+
+        public static final String TABLE_NAME  = "makes";
+
+        // column storing the name of the make as a string
+        public static final String COLUMN_NAME = "name";
+    }
+
+    /**
+     * All the constants defined for the cars table in the database
+     */
+    public static final class CarEntry implements BaseColumns {
+
+        public static final String TABLE_NAME  = "cars";
+
+        // column storing the name of the make as a string
+        // this corresponds to a make in the makes table
+        public static final String COLUMN_MAKE = "make";
+
+        // column storing the model of the car as a string
+        public static final String COLUMN_MODEL = "model";
+
+        // column storing the year of the car as an integer
+        public static final String COLUMN_YEAR = "year";
+    }
 }
