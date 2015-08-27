910211d8ab58f9d0a0b52c2e38a730c33b986b1b
Justin Ricks <justin.rixx@gmail.com>
2015-08-27 14:56:55 -0600

    Add URIs to the contract to access the tables and create a stubbed out implementation of a ContentProvider

--- app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Contract.java
+++ app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Contract.java
@@ -1,5 +1,6 @@
 package com.gmail.rixx.justin.contentprovidertest.Data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,13 +8,30 @@ import android.provider.BaseColumns;
  */
 public class Contract {
 
-    public static final String TABLE_MAKES = "makes";
-    public static final String TABLE_CARS  = "cars";
+    /**
+     * Content authority for the content provider. This is the package name
+     */
+    public static final String CONTENT_AUTHORITY = "com.gmail.rixx.justin.contentprovidertest.provider";
+
+    /**
+     * The base URI to fetch content from the content provider
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible paths from the base URI. These refer to their respective database tables
+     */
+    public static final String PATH_MAKES = "makes";
+    public static final String PATH_CARS = "cars";
 
     /**
      * All the constants defined for the makes table in the database
      */
     public static final class MakeEntry implements BaseColumns {
+
+        // The URI to access the makes table
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MAKES).build();
 
         public static final String TABLE_NAME  = "makes";
 
@@ -25,6 +43,10 @@ public class Contract {
      * All the constants defined for the cars table in the database
      */
     public static final class CarEntry implements BaseColumns {
+
+        // The URI to access the makes table
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_CARS).build();
 
         public static final String TABLE_NAME  = "cars";
 
---
+++ app/src/main/java/com/gmail/rixx/justin/contentprovidertest/Data/Provider.java
@@ -0,0 +1,99 @@
+package com.gmail.rixx.justin.contentprovidertest.Data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by justin on 8/27/15.
+ */
+public class Provider extends ContentProvider {
+
+    /* The integer codes for the various queries */
+    private static final int MAKES = 1;
+    private static final int MAKES_ID = 2;
+    private static final int CARS = 3;
+    private static final int CARS_ID = 4;
+    private static final int CARS_COLOR = 5;
+
+    /* Set up the URI matcher */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(Contract.CONTENT_AUTHORITY, Contract.MakeEntry.TABLE_NAME, MAKES);
+        sUriMatcher.addURI(Contract.CONTENT_AUTHORITY, Contract.MakeEntry.TABLE_NAME + "/#", MAKES_ID);
+        sUriMatcher.addURI(Contract.CONTENT_AUTHORITY, Contract.CarEntry.TABLE_NAME, CARS);
+        sUriMatcher.addURI(Contract.CONTENT_AUTHORITY, Contract.CarEntry.TABLE_NAME + "/#", CARS_ID);
+        sUriMatcher.addURI(Contract.CONTENT_AUTHORITY, Contract.CarEntry.TABLE_NAME + "/color/*", CARS_COLOR);
+    }
+
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        String table;
+
+        /* Figure out what to do based on the type of URI */
+        switch (sUriMatcher.match(uri)) {
+
+            case MAKES:
+                table = Contract.MakeEntry.TABLE_NAME;
+                break;
+
+            case MAKES_ID:
+                table = Contract.MakeEntry.TABLE_NAME;
+                selection = selection + Contract.MakeEntry._ID + " = " + uri.getLastPathSegment();
+                break;
+
+            case CARS:
+                table = Contract.CarEntry.TABLE_NAME;
+                break;
+
+            case CARS_ID:
+                table = Contract.CarEntry.TABLE_NAME;
+                selection = selection + Contract.CarEntry._ID + " = " + uri.getLastPathSegment();
+                break;
+
+            case CARS_COLOR:
+                table = Contract.CarEntry.TABLE_NAME;
+                selection = selection + Contract.CarEntry.COLUMN_MAKE + " = " + uri.getLastPathSegment();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid URI");
+
+        }
+
+        // TODO the actual query
+
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
